83de4c0884c9a60f0e01374700e71d1fdc1f7568
Ekoalfianto <131840198+ekoalfianto@users.noreply.github.com>
2024-09-04 11:25:09 +0700

    API resource

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Posts;
+use App\Http\Resources\PostsResource;
 
 class PostsController extends Controller
 {
     public function index(){
         $posts = Posts::all();
-        return response()->json(['data' => $posts]);
+
+        // return response()->json(['data' => $posts]);
+        return PostsResource::collection($posts);
     }
 }
