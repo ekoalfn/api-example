23d25b44866109e91dcdf9553f029ac659f3574e
Ekoalfianto <131840198+ekoalfianto@users.noreply.github.com>
2024-09-04 13:35:21 +0700

    sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\AuthenticationController;
 
-Route::get('/posts', [PostsController::class, 'index']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::get('/posts', [PostsController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/posts/{id}', [PostsController::class, 'show']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
